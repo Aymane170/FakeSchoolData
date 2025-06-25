e9d98945f0df361c930a5c52f6190a7ce064872c
AymaneR17 <arami@helb-prigogine.be>
2025-06-25 15:48:46 +0200

    rendu en anglais

--- generate_data.py
+++ generate_data.py
@@ -4,35 +4,35 @@ from faker import Faker
 import random
 
 
-fake = Faker()
+fake = Faker('fr_FR') 
 np.random.seed(42)
 random.seed(42)
 
 # Générer les cours
-nb_cours = 10
-cours = [{'id': i + 1, 'nom': fake.job()} for i in range(nb_cours)]
-df_cours = pd.DataFrame(cours)
-df_cours.to_csv('cours.csv', index=False)
+number_of_courses = 10
+courses = [{'id': i + 1, 'nom': fake.job()} for i in range(number_of_courses)]
+df_courses = pd.DataFrame(courses)
+df_courses.to_csv('courses.csv', index=False, encoding='utf-8-sig')
 
 
 # Générer les élèves
-nb_eleves = 50
-eleves = [{'id': i + 1, 'prenom': fake.first_name(), 'nom': fake.last_name()} for i in range(nb_eleves)]
-df_eleves = pd.DataFrame(eleves)
-df_eleves.to_csv('eleves.csv', index=False)
+number_of_students = 50
+students = [{'id': i + 1, 'firstname': fake.first_name(), 'lastname': fake.last_name()} for i in range(number_of_students)]
+df_students = pd.DataFrame(students)
+df_students.to_csv('students.csv', index=False, encoding='utf-8-sig')
 
 
 # Générer les résultats annuels
-resultats = []
-for eleve in eleves:
-    for cours in df_cours['id']:
-        note = round(np.random.normal(loc=12, scale=3), 2)  # moyenne autour de 12/20
-        note = max(0, min(20, note))  
-        resultats.append({
-            'id_eleve': eleve['id'],
-            'id_cours': cours,
-            'note': note
+results = []
+for student in students:
+    for courses in df_courses['id']:
+        grade = round(np.random.normal(loc=12, scale=3), 2)  # moyenne autour de 12/20
+        grade = max(0, min(20, grade))  
+        results.append({
+            'id_student': student['id'],
+            'id_courses': courses,
+            'grade': grade
         })
 
-df_resultats = pd.DataFrame(resultats)
-df_resultats.to_csv('resultats.csv', index=False)
+df_results = pd.DataFrame(results)
+df_results.to_csv('results.csv', index=False, encoding='utf-8-sig')
