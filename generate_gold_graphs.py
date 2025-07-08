1e2305171bb95b89a9abfd0c2da50521f1d47689
AymaneR17 <arami@helb-prigogine.be>
2025-07-08 17:43:50 +0200

    update connexion to github actions

--- generate_gold_graphs.py
+++ generate_gold_graphs.py
@@ -6,12 +6,12 @@ import snowflake.connector
 print("🔗 Connexion à Snowflake...")
 
 conn = snowflake.connector.connect(
-    user='AYMANE17',
+    user=os.getenv('SNOWFLAKE_USER'),
     password=os.getenv('SNOWFLAKE_PASSWORD'),
-    account='LKJZRTO-NM04387',
-    warehouse='COMPUTE_WH',
-    database='FakeSchool',
-    schema='RAW'
+    account=os.getenv('SNOWFLAKE_ACCOUNT'),
+    warehouse=os.getenv('SNOWFLAKE_WAREHOUSE'),
+    database=os.getenv('SNOWFLAKE_DATABASE'),
+    schema=os.getenv('SNOWFLAKE_SCHEMA')
 )
 
 cursor = conn.cursor()
