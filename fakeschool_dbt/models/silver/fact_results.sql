b0d047d81ba9a8bcc6eec56b6847f6fd5a169c53
AymaneR17 <arami@helb-prigogine.be>
2025-08-05 20:23:17 +0200

    upd

--- fakeschool_dbt/models/silver/fact_results.sql
+++ fakeschool_dbt/models/silver/fact_results.sql
@@ -1,3 +1,9 @@
+-- models/silver/results_cleaned.sql
+/*
+  Modèle results_cleaned : Résultats enrichis avec identifiants hashés
+  Jointures avec dim_students et dim_courses pour récupérer les clés uniques
+*/
+
 {{ config(
     materialized='incremental',
     unique_key='student_id || course_id'
@@ -30,4 +36,4 @@ select
     r.grade
 from results r
 left join students s on r.id_student = s.student_id
-left join courses c on r.id_course = c.course_id  -- attention ici : id_course au singulier si c’est le nom exact
+left join courses c on r.id_courses = c.course_id
